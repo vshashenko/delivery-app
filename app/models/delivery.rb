8a857132173a6183e5eda2b0633c2f200d7ae0b0
vshashenko <v.shashenko@gmail.com>
2024-12-24 13:58:50 +0100

    Updated server and client side validations

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,8 +1,16 @@
 class Delivery < ApplicationRecord
-    # TODO validations?
-    # validates :driver_name, presence: false
-    # address >= 3
-    # if html form sends empty string for driver, is it null here?
+  before_validation :convert_empty_attrs_to_nil
+
+  # Make sure to keep equal to client-side validations
   validates :pickup_address, :delivery_address, :weight, :distance, :scheduled_time, :cost, presence: true
-  validates :cost, numericality: { greater_than: 0 }
+  validates :weight, :distance, :cost, numericality: { greater_than: 0 }
+  validates :pickup_address, :delivery_address, length: { minimum: 3, maximum: 255 }
+  validates :driver_name, length: { minimum: 1, maximum: 255 }, allow_nil: true
+  # TODO validate scheduled_time not in the past?
+
+  private
+
+  def convert_empty_attrs_to_nil
+    self.driver_name = nil if self.driver_name.blank?
+  end
 end
